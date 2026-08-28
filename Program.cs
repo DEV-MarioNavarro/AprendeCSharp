d871372e9f3fcf7e4fe47e619dd4dd0953e3e8e2
Mario Navarro <m.navarro@traxion.global>
2026-08-28 12:27:04 -0600

    Mi primer proyecto en C#

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola Mario");
+Console.WriteLine("Tengo expeiencia en Genero");
+Console.WriteLine("Estoy aprendiendo C#");
+
+string material = "1001";
+string descripcion = "tornillo";
+decimal precio = 12.50m;
+
+Console.WriteLine("=== Maestro de Materiales ===");
+Console.WriteLine($"Material:  {material}");
+Console.WriteLine($"Descripcion: {descripcion}");
+Console.WriteLine($"Precio: {precio}");
